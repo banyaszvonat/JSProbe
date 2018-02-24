d441db333de7377f9cb9d7269bb395e92325c092
banyaszvonat <askdjfkj@example.com>
2018-02-24 23:04:04 +0100

    Sketch out library fingerprinter class

---
+++ library-fingerprinter.js
@@ -0,0 +1,27 @@
+// magic to identify various libraries goes here
+
+'use strict';
+
+var LibraryFingerprinter = {};
+
+
+// Entry point, passes off to sub-functions (e.g. determining library by pathname)
+// Returns a text identifier that the mapper can resolve to a local copy
+LibraryFingerprinter.libraryFromRequest = function (libraryRequest) {
+	console.log( arguments.callee.name + "not implemented yet");
+}
+
+LibraryFingerprinter.libraryFromPath = function (libraryPath) {
+	console.log( arguments.callee.name + "not implemented yet");
+}
+
+LibraryFingerprinter.libraryFromFeature = function (libraryFeature) {
+	console.log( arguments.callee.name + "not implemented yet");
+}
+
+
+// Helper functions 
+
+LibraryFingerprinter.extractPath = function (libraryRequest) {
+	console.log( arguments.callee.name + "not implemented yet");
+}
