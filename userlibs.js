a35faf1cd103de8139ba1624c19d5a684689dab3
banyaszvonat <askdjfkj@example.com>
2018-08-15 22:30:51 +0200

    Hooks don't pass arguments properly yet, but it's something

--- userlibs.js
+++ userlibs.js
@@ -6,7 +6,7 @@ var userlibs = {
 	// Special member: this is for signaling that the fingerprinter does not recognize the library
 	'UNKNOWN' : 'UNKNOWN',
 	'jquery' : {
-		'path': 'userlibs/jquery.js'
+		'path': 'userlibs/jquery-1.11.3-hooked.js'
 	}
 	
 };
